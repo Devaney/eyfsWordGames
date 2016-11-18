c3a527812f738caa60db800750463ad4d6b95942
Devaney <liamdevaney@live.co.uk>
2016-11-18 23:58:37 +0000

    Parsing through JSLint
    Hiding button for word timings as it is not working.

--- scripts/my.js
+++ scripts/my.js
@@ -1,9 +1,9 @@
-debug = false;
+debug = true;
 
 var selection_snd = new Audio("sounds/selection.mp3");
 var correct_snd = new Audio("sounds/correct.mp3");
-var wrong_snd = new Audio("sounds/wrong.mp3"); 
-var end_snd = new Audio("sounds/end-sound.mp3"); 
+var wrong_snd = new Audio("sounds/wrong.mp3");
+var end_snd = new Audio("sounds/end-sound.mp3");
 //var welcome_snd = new Audio("sounds/intro-music.wav");
 //welcome_snd.loop = true;
 //welcome_snd.play();
@@ -18,21 +18,21 @@ function getShuffledListOfWords() {
             if (rawFile.readyState === 4 && rawFile.status == "200") {
                 callback(rawFile.responseText);
             }
-        }
+        };
         rawFile.send(null);
-    };
-    
+    }
+
     function randomize(a, b) {
         return Math.random() - 0.5;
-    };
-    
+    }
+
     // Calling the data
     readTextFile("data/eyfsWords.json", function(text){
         words = JSON.parse(text);
         words.eyfsWords.sort(randomize);
     });//readTextFile
 
-};//getShuffledListOfWords
+}//getShuffledListOfWords
 
 function startGame(a) {
     wordStartCount = parseInt(a);
@@ -40,94 +40,96 @@ function startGame(a) {
     getNewWord();
     selection_snd.play();
     clearTimer();
-    timerStart();    
-};
+    timerStart();
+}
 
 function listWords() {
-    document.getElementById("intro-wrap").style.display = 'none';
-    document.getElementById("word-list-wrap").style.display = 'block';
-    for (var i = 0; i < words.eyfsWords.length ; i++) {
+    document.getElementById("intro-wrap").style.display = "none";
+    document.getElementById("word-list-wrap").style.display = "block";
+    for (var i = 0; i < words.eyfsWords.length; i++) {
         var rowColour = "table-danger";
-        if (words.eyfsWords[i].colour == "green"){rowColour = "table-success"};
+        if (words.eyfsWords[i].colour == "green"){
+          rowColour = "table-success";
+        }
         document.getElementById("word-list").innerHTML += "<tr class='" + rowColour + "'><th scope='row'>" + [i] + "</th><td><p class='"+ words.eyfsWords[i].colour + "'>" + words.eyfsWords[i].word + " </p></td><td><p class='"+ words.eyfsWords[i].colour + "'>" + words.eyfsWords[i].colour + " </p></td></tr>";
-    };
-};
+    }
+}
 
 function getNewWord() {
     roundSeconds = 0;
     roundTimeVar = setInterval(roundCountStart, 1000);
 
-    if (debug == true) {
+    if (debug === true) {
     document.getElementById("current-word").innerHTML = "<p class='"+ words.eyfsWords[correct_answers].colour + "'>" + words.eyfsWords[correct_answers].word + " </p>";
         } else{
           document.getElementById("current-word").innerHTML = "<p>" + words.eyfsWords[correct_answers].word + " </p>";
-    };
+    }
 
-    document.getElementById("intro-wrap").style.display = 'none';
-    document.getElementById("game-wrap").style.display = 'block';
+    document.getElementById("intro-wrap").style.display = "none";
+    document.getElementById("game-wrap").style.display = "block";
     document.getElementById("is_"+words.eyfsWords[correct_answers].colour).className += " correct_answer_btn";
     document.getElementById("is_red").onclick = answer;
     document.getElementById("is_green").onclick = answer;
-};
+}
 
 function roundCountStart() {
     ++roundSeconds;
     endRoundTime = roundSeconds;//FIXME Round time is incorrect for the words
-};
+}
 
 function answer() {
     if (this.classList.contains("correct_answer_btn")) {
         correct_snd.play();
-        addResultsTableRow('results-table', words.eyfsWords[correct_answers].word, endRoundTime);//FIXME endRoundTime uncaught referece error can casuse the first word to appear twice
+        addResultsTableRow("results-table", words.eyfsWords[correct_answers].word, endRoundTime);//FIXME endRoundTime uncaught referece error can casuse the first word to appear twice
         correct_answers++; //Add 1 to correct answers
-        document.getElementById('happy-face-animation').style.display='block';
-        setTimeout(function () {document.getElementById('happy-face-animation').style.display='none'}, 2000);
+        document.getElementById("happy-face-animation").style.display="block";
+        setTimeout(function () {document.getElementById("happy-face-animation").style.display="none";}, 2000);
     clearInterval(roundTimeVar);
         this.classList.remove("correct_answer_btn");
         if (correct_answers == words.eyfsWords.length) {
             endGame();
         } else{
         getNewWord();
-        };
+        }
     } else{
         wrong_snd.play();
-        document.getElementById('error-screen-block').style.display='block';
-        setTimeout(function () {document.getElementById('error-screen-block').style.display='none'}, 4000);
-    };
-}; // function answer()
+        document.getElementById("error-screen-block").style.display="block";
+        setTimeout(function () {document.getElementById("error-screen-block").style.display="none";}, 4000);
+    }
+} // function answer()
 
 function endGame() {
     usersTime = document.getElementById("timer").innerHTML;
-    document.getElementById("intro-wrap").style.display = 'block';
-    document.getElementById("game-wrap").style.display = 'none';
+    document.getElementById("intro-wrap").style.display = "block";
+    document.getElementById("game-wrap").style.display = "none";
     document.getElementById("initial-intro").innerHTML = "Well done<br>You did it in<br>" + usersTime;
     document.getElementById("initial-intro").style.fontSize = "34px";
     end_snd.play();
     stopTimer();
     getShuffledListOfWords();
     showResultsTable();
-};
+}
 
 function showResultsTable() {
-    document.getElementById("results-table-link").style.display = 'block';
-    document.getElementById("results-table").style.display = 'table';
-};
+    document.getElementById("results-table-link").style.display = "block";
+    document.getElementById("results-table").style.display = "table";
+}
 
 function showResultsTableBtn() {
-    document.getElementById("results-list-wrap").style.display = 'block';
-    document.getElementById("intro-wrap").style.display = 'none';
-};
+    document.getElementById("results-list-wrap").style.display = "block";
+    document.getElementById("intro-wrap").style.display = "none";
+}
 
 function restartGame() {
-    document.getElementById("intro-wrap").style.display = 'block';
-    document.getElementById("results-list-wrap").style.display = 'none';
-    document.getElementById("game-wrap").style.display = 'none';
-    document.getElementById("word-list-wrap").style.display = 'none';
+    document.getElementById("intro-wrap").style.display = "block";
+    document.getElementById("results-list-wrap").style.display = "none";
+    document.getElementById("game-wrap").style.display = "none";
+    document.getElementById("word-list-wrap").style.display = "none";
     document.getElementById("is_red").classList.remove("correct_answer_btn");
     document.getElementById("is_green").classList.remove("correct_answer_btn");
     stopTimer();
     clearTimer();
-};
+}
 
 function timerStart() {
     timerVar = setInterval(countStart, 1000);
@@ -136,18 +138,18 @@ function timerStart() {
         ++totalSeconds;
         var seconds = totalSeconds;
         document.getElementById("timer").innerHTML = seconds + " seconds";
-    };
-};
+    }
+}
 
 function clearTimer() {
     totalSeconds = 0;
     document.getElementById("timer").innerHTML = "0 seconds";
-};
+}
 
 function stopTimer() {
     clearInterval(timerVar);
     clearInterval(roundTimeVar);
-};
+}
 
 function addResultsTableRow(tableID, cellOne, cellTwo) {
     // Get a reference to the table
@@ -161,22 +163,22 @@ function addResultsTableRow(tableID, cellOne, cellTwo) {
     var secondNewCell = newRow.insertCell(1);
     var secondNewText = document.createTextNode(cellTwo);
     secondNewCell.appendChild(secondNewText);
-};
+}
 
 //sorting the results table
 // // Table data sorting starts....
 //   function sortData() {
 //       // Read table body node.
-//       var tableData = document.getElementById('data_table').getElementsByTagName('tbody').item(0);
+//       var tableData = document.getElementById("data_table").getElementsByTagName("tbody").item(0);
 
 //       // Read table row nodes.
-//       var rowData = tableData.getElementsByTagName('tr'); 
+//       var rowData = tableData.getElementsByTagName("tr");
 
 //       for(var i = 0; i < rowData.length - 1; i++) {
 //           for(var j = 0; j < rowData.length - (i + 1); j++) {
 
 //               //Swap row nodes if short condition matches
-//               if(parseInt(rowData.item(j).getElementsByTagName('td').item(0).innerHTML) > parseInt(rowData.item(j+1).getElementsByTagName('td').item(0).innerHTML)) {
+//               if(parseInt(rowData.item(j).getElementsByTagName("td").item(0).innerHTML) > parseInt(rowData.item(j+1).getElementsByTagName("td").item(0).innerHTML)) {
 //                   tableData.insertBefore(rowData.item(j+1),rowData.item(j));
 //               }
 //           }
